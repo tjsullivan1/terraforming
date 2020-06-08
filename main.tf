d5e5043508b830fc87d2c1b119c7a92a4d81a807
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-08 19:41:16 +0000

    testing new vm

--- main.tf
+++ main.tf
@@ -1,19 +1,67 @@
 # Configure the Azure provider
 provider "azurerm" {
     version = "=2.13.0"
+    region = "eastus"
     features {}
 }
 
 # Create a new resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
+    name     = "rg-terraform-learning"
     location = "eastus"
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
+    name                = "vnet-tf"
     address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
+    location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "snet-internal" {
+  name                 = "snet-internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet-internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
 }
