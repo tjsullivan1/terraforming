e20f1fd67b21977b9df18ab64993b2d5cc935ad4
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-08 19:08:43 +0000

    testing tfcloud

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Configure the Azure provider
+provider "azurerm" {
+    version = "=2.13.0"
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroup"
+    location = "eastus"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
