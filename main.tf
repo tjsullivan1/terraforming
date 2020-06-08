cfb2f34ae84a5232d8bb43e55cef1cf1abf6a808
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-08 19:56:35 +0000

    20.04 not available yet

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
