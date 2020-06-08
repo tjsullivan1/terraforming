97dffbec9bff3c522c29be3ff21c05dade1542bb
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-08 19:46:07 +0000

    converted address_prefixes to list

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "snet-internal" {
   name                 = "snet-internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes       = "10.0.2.0/24"
+  address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
