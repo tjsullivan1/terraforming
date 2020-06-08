310079e7763575abf77233020d38ed20c5a3abdb
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-08 21:40:42 +0000

    added nsg

--- main.tf
+++ main.tf
@@ -75,7 +75,29 @@ resource "azurerm_virtual_machine_extension" "vmext" {
 
     settings = <<PROT
     {
-        "commandToExecute": "echo 'Hello, World' > index.html &&nohup busybox httpd -f -p 8080 &"
+        "commandToExecute": "echo 'Hello, World' > index.html && nohup busybox httpd -f -p 8080 &"
     }
     PROT
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "nsg-web-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "port8080"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
