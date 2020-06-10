707979ac1d9118eeb0ff1ec3d33cfdcde20147f4
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-10 17:21:37 +0000

    forgot to rename repo_name variable

--- modules/global/tf-cloud/vars.tf
+++ modules/global/tf-cloud/vars.tf
@@ -3,13 +3,13 @@ variable "workspace_name" {
  type = string
 }
 
-variable "name" {
+variable "repo_name" {
   description = "The GitHub relative path to the Terraform Repository"
   type = string
 }
 
 variable "component_path" {
-  description = "The path within the repository where the code is stored for this particular deployment."
+  description = "The path within the repository where the code is stored for this particular"
   default = "/"
 }
 
