ba1e52f5771c5a31a2d9109da4e7c4372526142f
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-12 14:41:38 +0000

    Added module for webserver

---
+++ modules/services/webserver/main.tf
@@ -0,0 +1,115 @@
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-terraform-learning"
+    location = "eastus"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-tf"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "snet-internal" {
+  name                 = "snet-internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet-internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.instance_sku
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = var.ssh_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vmext" {
+    name                    = "vmext-${var.name}"
+
+    virtual_machine_id = azurerm_linux_virtual_machine.vm1.id
+    publisher            = "Microsoft.Azure.Extensions"
+    type                 = "CustomScript"
+    type_handler_version = "2.0"
+
+    settings = <<PROT
+    {
+        "commandToExecute": "echo 'Hello, World' > index.html && nohup busybox httpd -f -p ${var.server_port} &"
+    }
+    PROT
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-web-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "webserverlistener"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = var.server_port
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-${var.name}""
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vmToNsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
