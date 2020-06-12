ad4e27ec0970cffa52c90fa2dc355ff57804f09c
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-12 14:50:05 +0000

    had an extra quote in the module main.tf for webserver

--- modules/services/webserver/main.tf
+++ modules/services/webserver/main.tf
@@ -99,7 +99,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-${var.name}""
+  name                = "pip-${var.name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
