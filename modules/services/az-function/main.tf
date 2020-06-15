182a2b13d272f72464a7dbb51d75cb866bd89846
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-15 21:00:17 +0000

    trying again

--- modules/services/az-function/main.tf
+++ modules/services/az-function/main.tf
@@ -15,7 +15,9 @@ resource "azurerm_app_service_plan" "asp" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   kind                = "FunctionApp"
-  reserved            = "%{if var.os == "linux"} true %{else} false %{endif}"
+  reserved            = (var.os == "linux"
+                        ? true 
+                        : false)
 
   sku {
     tier = var.function_tier
