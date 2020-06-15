160dedb9bc12553441c1b8e81eae71acf5336111
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-15 21:04:26 +0000

    adding count to function app because I can't get OSType to work with windows

--- modules/services/az-function/main.tf
+++ modules/services/az-function/main.tf
@@ -15,9 +15,9 @@ resource "azurerm_app_service_plan" "asp" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   kind                = "FunctionApp"
-  reserved            = (var.os == "linux"
-                        ? true 
-                        : false)
+  reserved = (var.os == "linux"
+    ? true
+  : false)
 
   sku {
     tier = var.function_tier
@@ -32,11 +32,27 @@ resource "azurerm_application_insights" "appin" {
   application_type    = "web"
 }
 
-resource "azurerm_function_app" "function" {
+resource "azurerm_function_app" "function_lin" {
+  count = (var.os == "linux"
+    ? 1
+  : 0)
+  
   name                      = "azfun-${var.name}-${var.env}"
   location                  = data.azurerm_resource_group.rg.location
   resource_group_name       = data.azurerm_resource_group.rg.name
   app_service_plan_id       = azurerm_app_service_plan.asp.id
   storage_connection_string = azurerm_storage_account.funcstore.primary_connection_string
   os_type                   = var.os
+}
+
+resource "azurerm_function_app" "function_win" {
+  count = (var.os == "linux"
+    ? 0
+  : 1)
+
+  name                      = "azfun-${var.name}-${var.env}"
+  location                  = data.azurerm_resource_group.rg.location
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  app_service_plan_id       = azurerm_app_service_plan.asp.id
+  storage_connection_string = azurerm_storage_account.funcstore.primary_connection_string
 }
