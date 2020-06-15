2d3a22702fc2830f275aba382e426dae8eddca9c
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-15 19:44:05 +0000

    added linker-parser function

---
+++ dev/services/linker-parser/main.tf
@@ -0,0 +1,19 @@
+# Configure the Azure provider
+provider "azurerm" {
+    version = "=2.13.0"
+    features {}
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-linker-parser"
+    location = "eastus"
+}
+
+module "webserver" {
+  source = "github.com/tjsullivan1/terraforming/modules/services/az-function"
+
+  env = "d"
+  name = "linkpar"
+
+}
