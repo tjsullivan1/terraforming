ea0572d377a9c0ab14ae28258a100706d2e2f57f
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-06-15 21:27:28 +0000

    trying to add a depends on

--- dev/services/linker-parser/main.tf
+++ dev/services/linker-parser/main.tf
@@ -1,20 +1,21 @@
 # Configure the Azure provider
 provider "azurerm" {
-    version = "=2.13.0"
-    features {}
+  version = "=2.13.0"
+  features {}
 }
 
 # Create a new resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-linker-parser"
-    location = "eastus"
+  name     = "rg-linker-parser"
+  location = "eastus"
 }
 
 module "webserver" {
-  source = "github.com/tjsullivan1/terraforming/modules/services/az-function"
+  depends_on = [azurerm_resource_group.rg]
+  source     = "github.com/tjsullivan1/terraforming/modules/services/az-function"
 
-  env = "d"
-  name = "linkpar"
+  env                 = "d"
+  name                = "linkpar"
   resource_group_name = azurerm_resource_group.rg.name
 
 }
